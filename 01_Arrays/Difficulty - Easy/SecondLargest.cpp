03d0fe3babc825081aa7c040d6bea3018ca3d4da
Danish Khan <dk831675@gmail.com>
2025-02-01 09:04:34 +0530

    commit changes

--- 01_Arrays/Difficulty - Easy/SecondLargest.cpp
+++ 01_Arrays/Difficulty - Easy/SecondLargest.cpp
@@ -1,6 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 void SecondLargest(int arr[], int size){
+    int largest = arr[0];
+    int second_largest = INT_MIN;
+    for(int i=0; i<size; i++){
+        if(arr[i] > largest){
+            second_largest = largest;
+            largest = arr[i];
+        }else if(arr[i] < largest && arr[i] > second_largest){
+            second_largest = arr[i];
+        }
+    }
+    cout<<"Second Largest Element is: "<<second_largest<<endl;
+}
+int main(){
+    int arr[5] = {40,20,30,60,43};
+    int size = 5;
+
+    SecondLargest(arr,size);
+    
+    return 0;
+}
+
+
+/*  Better approach, but not optimal
+void SecondLargest(int arr[], int size){
     //first parsing and storing largest element
     int firstLargest = arr[0];
     for(int i=0; i<size; i++){
@@ -19,11 +43,4 @@ void SecondLargest(int arr[], int size){
 
     cout<<"Second Largest element in array is: "<<secondLargest<<endl;
 }
-int main(){
-    int arr[5] = {40,20,30,60,43};
-    int size = 5;
-
-    SecondLargest(arr,size);
-    
-    return 0;
-}
+*/
--- 01_Arrays/Difficulty - Easy/SecondSmallest.cpp
+++ 01_Arrays/Difficulty - Easy/SecondSmallest.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 void secondSmallest(int arr[], int size){
+    int smallest = arr[0];
+    int second_smallest = INT_MAX;
+    for(int i=0; i<size; i++){
+        if(arr[i] < smallest){
+            second_smallest = smallest;
+            smallest = arr[i];
+        }else if(arr[i]!=smallest && arr[i] < second_smallest){
+            second_smallest = arr[i];
+        }
+    }
+    cout<<"Second smallest element is: "<<second_smallest<<endl;
+}
+int main(){
+    int arr[5] = {12,30,9,32,71};
+    int size = 5;
+
+    secondSmallest(arr,size);
+
+    return 0;
+}
+
+/* Better appraoch, but not optimal
+void secondSmallest(int arr[], int size){
     //first parsing, checking for first smallest element
     int firstSmallest = arr[0];
     for(int i=0; i<size; i++){
@@ -19,11 +42,4 @@ void secondSmallest(int arr[], int size){
 
     cout<<"Second Smallest element in array is: "<<secondSmallest<<endl;
 }
-int main(){
-    int arr[5] = {12,30,9,32,71};
-    int size = 5;
-
-    secondSmallest(arr,size);
-
-    return 0;
-}
+*/
